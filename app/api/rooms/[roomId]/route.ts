afd76b41ce21547ff3fdbbdc4d63927cf96659a5
princegupta-in <zprincegupta@gmail.com>
2025-03-02 16:17:16 +0530

    error: should be awaited before using its properties, fixed

--- app/api/rooms/[roomId]/route.ts
+++ app/api/rooms/[roomId]/route.ts
@@ -3,16 +3,18 @@ import { prisma } from '@/lib/connectToDatabase';
 
 export async function GET(
     request: Request,
-    { params }: { params: { roomId: string } }
+    { params }: { params: { roomId: string  } }
 ) {
     try {
+        const { roomId } = await params
+
         const room = await prisma.room.findUnique({
-            where: { id: params.roomId },
+            where: { id: roomId },
         });
         if (!room) {
             return NextResponse.json({ error: 'Room not found' }, { status: 404 });
         }
-        return NextResponse.json(room, { status: 200 });
+        return NextResponse.json({ room }, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }

fb5aaa10bb51d09cb7abdde778a1d3c31827a408
princegupta-in <zprincegupta@gmail.com>
2025-03-15 21:43:18 +0530

    added credentials provider

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,6 +1,8 @@
 import { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "./connectToDatabase";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
 
 export const authOptions: NextAuthOptions = {
@@ -8,6 +10,30 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        }),
+
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "prince@gmail.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials?.email }
+                });
+                if (!user) {
+                    throw new Error("Provided email/username does not exist");
+                } else {
+                    // Verify the password
+                    const isValidPassword = await bcrypt.compare(credentials!.password, user.password!);
+                    if (isValidPassword) {
+                        return user;
+                    } else {
+                        throw new Error("Incorrect Password");
+                    }
+                }
+            }
         })
     ],
     callbacks: {
@@ -23,33 +49,35 @@ export const authOptions: NextAuthOptions = {
             return token
         },
 
-        async signIn({ user }) {
-            const { email, name, image,id } = user;
-            // console.log("🌸🌸",user)
+        async signIn({ user, account }) {
+            //only run for google provider to store data
+            if (account?.provider === "google") {
+                const { email, name, image, id } = user;
+                // console.log("🌸🌸",user)
 
-            if (!email) return false; // Ensure email is present
+                if (!email) return false; // Ensure email is present
 
-            // Check if user already exists
-            const existingUser = await prisma.user.findUnique({
-                where: { email },
-            });// If user does not exist, create a new record
-            if (!existingUser) {
-                await prisma.user.create({
-                    data: {
-                        email,
-                        id,
-                        provider: "Google",
-                    },
-                });
+                // Check if user already exists
+                const existingUser = await prisma.user.findUnique({
+                    where: { email },
+                });// If user does not exist, create a new record
+                if (!existingUser) {
+                    await prisma.user.create({
+                        data: {
+                            email,
+                            id,
+                            provider: "Google",
+                        },
+                    });
+                }
             }
-
             return true; // Allow sign in
         },
     },
-    // pages: {
-    //     signIn: "/login",
-    //     error: "/login"
-    // },
+    pages: {
+        signIn: "/sign-in",
+        error: "/sign-in"
+    },
     session: {
         strategy: "jwt",
         maxAge: 10 * 24 * 60 * 60,
